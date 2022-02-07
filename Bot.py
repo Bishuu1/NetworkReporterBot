993759727e26379ede10bda4f65801a58d25021e
observium_admin <observium_admin@observium.novalte.corp>
2022-02-07 15:53:27 -0300

    actualizaciòn

--- Bot.py
+++ Bot.py
@@ -1,24 +1,23 @@
 from netmiko import ConnectHandler
 
-huawei = {
+huawei1 = {
     'device_type': 'huawei',
-    'ip': 'AQUI_VA_LA_IP',
-    'user': '',
-    'pass': '',
-    'port': 123,  # optional
-    'secret:': 'secret'
+    'host': '10.120.112.2',
+    'username': 'unimus',
+    'password': '5n3m5s_w4m',
+    'port': 22  # optional
 }
 
-net_connect = ConnectHandler(**huawei)
+net_connect = ConnectHandler(**huawei1)
 
 net_connect.enable()
 
-list = ['display version', 'display mac-address', 'display arp', 'display ip routing-table',
-        'display ip routing-table all-vpn-instance', 'display interface brief', 'display ip interface brief',
-        'display ospf peer brief', 'display ospf interface']
+list =  ['display mac-address'] #', 'display arp', 'display ip routing-table',
+        #'display ip routing-table all-vpn-instance', 'display interface brief', 'display ip interface brief',
+        #'display ospf peer brief', 'display ospf interface']'''
 
 for command in list:
     output = net_connect.send_command(command)
-    f = open(command+".txt", "w+")
+    f = open('prueba.txt', 'w+')
     f.write(output)
     f.close()
