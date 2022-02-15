b9fea4f4171bc0842d23ae27736d5c5a2daea7b3
observium_admin <observium_admin@observium.novalte.corp>
2022-02-15 13:48:56 -0300

    final e implentado

--- Bot.py
+++ Bot.py
@@ -1,25 +1,52 @@
+import os
+import schedule
+import time
+from datetime import datetime, timedelta 
 from netmiko import ConnectHandler
+from maquinas import devices
 
-huawei1 = {
-    'device_type': '',
-    'host': '',
-    'username': '',
-    'password': '',
-    'port':   # optional
-}
+def job():
+    for ip in devices:  
+        parent_directory_date = '/home/observium_admin/reportes'
+        day_directory = datetime.today().strftime('%A')
+        day_path = os.path.join(parent_directory_date,day_directory) #Directory for everyday report
+        try:
+            os.mkdir(day_path)
+            print('the directory has been created')
+        except OSError as error:
+            print(error)
+
+        parent_directory = day_path
+        directory = ip['host']
+        path = os.path.join(parent_directory,directory) #Directory for every machine
+        try:
+            os.mkdir(path)
+            print('the directory has been created')
+        except OSError as error:
+            print(error)
+
+        net_connect = ConnectHandler(**ip)
+        net_connect.enable()
+
+
+        list =  ['display version', 'display mac-address', 'display arp',
+                'display interface brief', 'display ip interface brief',
+                'display ospf peer brief', 'display ospf interface']
+
+        for command in list:
+            output = net_connect.send_command(command)
+            f_name = "display logbuffer startime yesterday.txt" if command == logbuffer else command+'.txt'
+            complete_name = os.path.join(path,f_name) #Path to create the files
+            f = open(complete_name,'w+')
+            f.write(output)
+            f.close()
+        net_connect.disconnect
+    print('Reports had successfully created')
 
 
 
-net_connect = ConnectHandler(**huawei1)
+schedule.every().day.at("01:00").do(job)
 
-net_connect.enable()
-
-list =  ['display mac-address'] #', 'display arp', 'display ip routing-table',
-        #'display ip routing-table all-vpn-instance', 'display interface brief', 'display ip interface brief',
-        #'display ospf peer brief', 'display ospf interface']'''
-
-for command in list:
-    output = net_connect.send_command(command)
-    f = open('prueba.txt', 'w+')
-    f.write(output)
-    f.close()
+while True:
+    schedule.run_pending()
+    time.sleep(30)
